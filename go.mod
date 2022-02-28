abb6cde18b9bd5b8fa53e4b1d9781bd36ba849f9
Andrejs Cainikovs <andrejs.cainikovs@gmail.com>
2022-02-28 11:31:03 +0100

    Implement reading from Google Cloud Storage

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/dieselburner/pongo2-gcloud
 
-go 1.17
+go 1.16
+
+require (
+	cloud.google.com/go/compute v1.5.0
+	cloud.google.com/go/iam v0.2.0 // indirect
+	cloud.google.com/go/storage v1.21.0
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
+	golang.org/x/oauth2 v0.0.0-20220223155221-ee480838109b // indirect
+	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
+)
--- pongo2gcloud.go
+++ pongo2gcloud.go
@@ -1,8 +1,13 @@
 package pongo2gcloud
 
 import (
-	"fmt"
+	"context"
 	"io"
+	"log"
+	"path"
+
+	"cloud.google.com/go/compute/metadata"
+	"cloud.google.com/go/storage"
 )
 
 // Loader implements pongo2's TemplateLoader interface for templates stored on Google Cloud Storage
@@ -13,12 +18,43 @@ type Loader struct {
 // Abs calculates the path to a given template. Whenever a path must be resolved
 // due to an import from another template, the base equals the parent template's path.
 func (l *Loader) Abs(base, name string) string {
-	fmt.Print("Abs: base=", base, " / name=", name)
-	return name
+	dir, _ := path.Split(base)
+	return path.Join(dir, name)
 }
 
 // Get returns an io.Reader where the template's content can be read from.
-func (l *Loader) Get(path string) (io.Reader, error) {
-	fmt.Print("Get: path=", path)
-	return nil, fmt.Errorf("not implemented")
+func (l *Loader) Get(name string) (io.Reader, error) {
+	var err error
+	var project_id string
+
+	// If ProjectID is not set, try to fetch it from Google Cloud Engine
+	if len(l.ProjectID) > 0 {
+		project_id = l.ProjectID
+	} else {
+		if !metadata.OnGCE() {
+			log.Fatalf("ProjectID needs to be set when running outside of Google Cloud Engine")
+		}
+		project_id, err = metadata.ProjectID()
+		if err != nil {
+			log.Fatalf("Could not fetch Google Compute Engine metadata: %v", err)
+		} else if project_id == "" {
+			log.Fatalf("Could not obtain Google Gloud project ID")
+		}
+	}
+
+	// Create Google Cloud Storage client
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Fatalf("Could not create Google Cloud Storage Client")
+	}
+
+	// Get template file from project bucket
+	reader, err := client.Bucket(project_id + ".appspot.com").Object(name).NewReader(ctx)
+	if err != nil {
+		log.Fatalf("Could not get template file '%s' from bucket: %v", name, err)
+	}
+
+	// Done
+	return reader, nil
 }
